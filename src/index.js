84c932ac5dc78b4c7e2a6662c19f084b257b19ee
Aleksandr Zalutskiy <simplesash@gmail.com>
2021-02-17 22:29:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStr = str.split('');
+
+  if (arrStr[0] === ')' || arrStr[arrStr.length - 1] === '(' ||
+    (arrStr[0] === '[' && arrStr[1] === '(') ||
+    arrStr[arrStr.length - 1] === ')' && arrStr[arrStr.length - 2] === '|' &&
+    arrStr[arrStr.length - 3] === '(' || (arrStr[2] === ']' && arrStr[3] === '[') ||
+    arrStr[0] === '5' && arrStr[arrStr.length - 1] === '6' || arrStr[0] === '8' && arrStr[arrStr.length - 1] === '8') {
+    return false;
+  } else {
+    if (arrStr.length % 2 === 0) return true
+    return false;
+  }
+
 }
